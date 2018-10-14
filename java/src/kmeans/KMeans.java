cae74ba35b2b3276be90ea02d0c74e69e09fcc7b
DavidPerezGomez <davidperezgo@gmail.com>
2018-10-14 22:14:27 +0200

    improved kmeans in java

--- java/src/kmeans/KMeans.java
+++ java/src/kmeans/KMeans.java
@@ -4,13 +4,13 @@ import utils.Utils;
 import weka.core.*;
 
 import java.util.Arrays;
-import java.util.Enumeration;
 import java.util.Random;
 
 public class KMeans {
 
     private Instance[] instances;
     private Instance[] centroids;
+    private int[] centroidsInstances;
     private Instance[] prevCentroids;
     private boolean[][] belongingBits;
     private int k;
@@ -20,94 +20,62 @@ public class KMeans {
 
 
     public static void main(String[] args) {
-//        Instances instances = Utils.loadInstances("/home/david/Escritorio/test.arff", 0);
-        Instances instances = Utils.loadInstances("/media/david/data/Shared/Documentos/Universidad/4º/Minería de Datos/Proyecto/files/verbal_autopsies_raw_tfidf.arff", 0);
+        Instances instances = Utils.loadInstances("/home/david/Documentos/Universidad/4º/Minería de Datos/Proyecto/files/verbal_autopsies_raw_tfidf.arff", 0);
         System.out.println(instances.numInstances());
-        KMeans kmeans = new KMeans(instances, 2);
-        kmeans.formClusters();
+        KMeans kmeans = new KMeans(instances, 96, "9-last");
+        kmeans.formClusters(true);
     }
 
-    private void test() {
-        Instances instances = Utils.loadInstances("/home/david/Escritorio/test.arff", 0);
-        for (int i = 0; i < instances.numAttributes(); i++) {
-            Attribute attr = instances.attribute(i);
-            Enumeration<Object> vals = attr.enumerateValues();
-            while (vals != null && vals.hasMoreElements()) {
-                System.out.print(vals.nextElement());
-                if (vals.hasMoreElements())
-                    System.out.print(", ");
-                else
-                    System.out.println();
-
-            }
-        }
-        for (int i = 0; i < instances.numInstances(); i++) {
-            Instance ins = instances.get(i);
-            for (int j = 0; j < ins.numAttributes(); j++) {
-                System.out.print(ins.value(j));
-                if (j != ins.numAttributes() - 1)
-                    System.out.print(", ");
-                else
-                    System.out.println();
-            }
-        }
-        System.out.println("\nDstances");
-        EuclideanDistance distance = new EuclideanDistance();
-        distance.setInstances(instances);
-        for (int i = 0; i < instances.numInstances(); i++) {
-            for (int j = 0; j < instances.numInstances(); j++) {
-                Instance insA = instances.get(i);
-                Instance insB = instances.get(j);
-                System.out.println(i + " - " + j + " -> " + distance.distance(insA, insB));
-            }
-        }
-        System.out.println("\nSums");
-        for (int i = 0; i < instances.numInstances(); i++) {
-            for (int j = 0; j < instances.numInstances(); j++) {
-                Instance insA = instances.get(i);
-                Instance insB = instances.get(j);
-                System.out.println(i + " - " + j + " -> " + KMeans.addInstances(insA, insB));
-            }
-        }
+    public KMeans(Instances pInstances, int pClusters, String pAttrRange) {
+        this(pInstances, pClusters, 0.1, pAttrRange);
     }
 
-    public KMeans(Instances pInstances, int pClusters) {
-        this(pInstances, pClusters, 0.1);
-    }
-
-    public KMeans(Instances pInstances, int pClusters, double pTolerance) {
+    public KMeans(Instances pInstances, int pClusters, double pTolerance, String pAttrRange) {
         this.instances = new Instance[pInstances.numInstances()];
         for (int i = 0; i < instances.length; i++) {
             this.instances[i] = pInstances.get(i);
         }
         this.k = Math.min(pClusters, this.instances.length);
         this.centroids = new Instance[this.k];
+        this.centroidsInstances = new int[k];
         this.prevCentroids = new Instance[this.k];
         this.belongingBits = new boolean[this.instances.length][this.k];
         this.tolerance = pTolerance;
+        Range attrRange = new Range();
+        attrRange.setRanges(pAttrRange);
+        attrRange.setUpper(pInstances.numAttributes() - 1);
         this.distance = new EuclideanDistance();
         this.distance.setInstances(pInstances);
-        this.maxIt = 100;
+        this.distance.setAttributeIndices(pAttrRange);
+        this.maxIt = 50;
     }
 
     public void formClusters() {
+        this.formClusters(false);
+    }
+
+    public String formClusters(boolean pVerbose) {
         // initialize centroids to random instances
         this.initializeCentroids();
         int it = 0;
         // iterate until centroids converge
         while (it < this.maxIt && !this.checkFinished()) {
             it++;
+            if (pVerbose)
+                System.out.println("It: " + it);
             // update belonging matrix
             this.updateBelongingBits();
-            System.out.println("It: " + it);
-            this.print_status();
             // obtain new centroids
             this.updateCentroids();
-            System.out.print("updated centroids");
+            if (pVerbose)
+                System.out.println(String.format("Iteration nº %d finished", it));
         }
         // obtain final belonging matrix
         this.updateBelongingBits();
-        this.print_status();
+        String clusters = this.getClusters();
+        if (pVerbose)
+            System.out.print(clusters);
+        return clusters;
     }
 
     private void initializeCentroids() {
@@ -119,6 +87,32 @@ public class KMeans {
 
             } while (Arrays.asList(this.centroids).contains(centroid));
             centroids[i] = centroid;
+        }
+    }
+
+    private void updateBelongingBits() {
+        for (int c = 0; c < this.belongingBits.length; c++) {
+            for (int r = 0; r < this.belongingBits[0].length; r++) {
+                this.belongingBits[c][r] = false;
+            }
+        }
+        for (int t = 0; t < this.instances.length; t++) {
+            double min_diff = 99999;
+            int min_i = -1;
+            Instance instance = this.instances[t];
+            for (int i = 0; i < this.centroids.length; i++) {
+                Instance centroid = this.centroids[i];
+                double diff = Arrays.equals(instance.toDoubleArray(), centroid.toDoubleArray()) ? 0 : distance.distance(instance, centroid);
+                if (diff < min_diff) {
+                    min_diff = diff;
+                    min_i = i;
+                } else if (diff == min_diff && this.centroidsInstances[i] < this.centroidsInstances[min_i]) {
+                    min_diff = diff;
+                    min_i = i;
+                }
+            }
+            this.belongingBits[t][min_i] = true;
+            this.centroidsInstances[min_i]++;
         }
     }
 
@@ -138,37 +132,22 @@ public class KMeans {
                 }
             }
             this.centroids[i] = divideInstance(instance_sum, bits_i);
-        }
-    }
-
-    private void updateBelongingBits() {
-        for (int c = 0; c < this.belongingBits.length; c++) {
-            for (int r = 0; r < this.belongingBits[0].length; r++) {
-                this.belongingBits[c][r] = false;
-            }
-        }
-        for (int t = 0; t < this.instances.length; t++) {
-            double min_diff = 99999;
-            int min_i = -1;
-            for (int i = 0; i < this.centroids.length; i++) {
-                double diff = this.distance.distance(this.instances[t], this.centroids[i]);
-                if (diff < min_diff) {
-                    min_diff = diff;
-                    min_i = i;
-                }
-            }
-            this.belongingBits[t][min_i] = true;
+            this.centroidsInstances[i] = 0;
         }
     }
 
     private boolean checkFinished() {
-        boolean finished = true;
+        if (prevCentroids[0] == null) {
+            return false;
+        }
         for (int i = 0; i < this.centroids.length; i++) {
             Instance centroid = this.centroids[i];
             Instance prev = this.prevCentroids[i];
-            finished = prev != null && distance.distance(centroid, prev) < this.tolerance;
+            if (distance.distance(centroid, prev) > this.tolerance) {
+                return false;
+            }
         }
-        return finished;
+        return true;
     }
 
     private static Instance addInstances(Instance pInstanceA, Instance pInstanceB) {
@@ -203,15 +182,45 @@ public class KMeans {
         return res;
     }
 
-    private void print_status() {
-        System.out.println(String.format("k: %d", this.k));
-        System.out.println("Instances: ");
-        for (int t = 0; t < this.instances.length; t++) {
-            System.out.println(String.format("\tt: %d", t));
-            for (int i = 0; i < this.centroids.length; i++) {
-                System.out.println(String.format("\t\ti: %d -> %s", i, this.belongingBits[t][i]));
+    private String getClusters() {
+        StringBuilder clusters = new StringBuilder();
+        clusters.append(String.format("k: %d\n", this.k));
+        int attrToPrint = 4;
+        for (int i = 0; i < this.centroids.length; i++) {
+            clusters.append(String.format("CLUSTER %d\n", i));
+            clusters.append(String.format("\tcentroid: %s\n", instanceToString(this.centroids[i], attrToPrint)));
+            clusters.append("\tinstances:\n");
+            for (int t = 0; t < this.instances.length; t++) {
+                if (this.belongingBits[t][i])
+                    clusters.append(String.format("\t\t%s\n", instanceToString(this.instances[t], attrToPrint)));
             }
         }
+        return clusters.toString();
     }
 
+    private static String instanceToString(Instance pInstance, int pAttributes) {
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < pAttributes; i++) {
+            Attribute attr = pInstance.attribute(i);
+            if (attr.type() == Attribute.NUMERIC) {
+                res.append(pInstance.value(i));
+            } else {
+                res.append(attr.value((int) pInstance.value(i)));
+            }
+            if (i < pAttributes -1) {
+                res.append(", ");
+            }
+        }
+        return res.toString();
+    }
+
+    private void saveCentroids(String pPath, boolean pPrevious) {
+        Instances centroids = new Instances(this.centroids[0].dataset());
+        centroids.delete();
+        centroids.addAll(Arrays.asList(this.centroids));
+        if (pPrevious) {
+            centroids.addAll(Arrays.asList(this.prevCentroids));
+        }
+        Utils.saveInstances(centroids, pPath);
+    }
 }
