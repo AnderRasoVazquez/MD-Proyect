c9834fec4c916e0ce205bd0f19966d2b018ed628
Ander Raso Vázquez <anderraso@gmail.com>
2018-10-15 16:23:22 +0200

    notebooks subidos con los análisis de los datos y añadido al csv

--- python/src/csv_cleaner.py
+++ python/src/csv_cleaner.py
@@ -1,6 +1,24 @@
 import sys
 import csv
+import string
+import nltk
+import itertools
+
 from utils import utils
+from nltk.stem.porter import PorterStemmer
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+
+
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt')
+
+try:
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download('stopwords')
 
 
 def main():
@@ -34,9 +52,35 @@ def main():
                 writer.writerow(new_row)
 
 
-def clean_string(string):
-    new_string = string.replace('_x000D_', '').replace('\n', '')
-    new_string = new_string.replace('/', ' ')
+def clean_string(t, use_stemmer):
+
+    t = string.replace('_x000D_', '').replace('\n', '')
+    t = new_string.replace('/', ' ')
+
+    #sentences = sent_tokenize(t)
+    # Genera un array de palabras (word_tokenize)
+    tokens = word_tokenize(t)
+    # Convierte las palabras en minúsculas(to lower case)
+    tokens = [w.lower() for w in tokens]
+    # Elimina signos de puntuación
+    table = str.maketrans('', '', string.punctuation)
+    stripped = [w.translate(table) for w in tokens]
+
+
+    # Elimina símbolos no alfabéticos
+    words = [word for word in stripped if word.isalpha()]
+
+    # Filtra preposiciones (stop-words)
+    stop_words = set(stopwords.words('english'))
+    words = [w for w in words if not w in stop_words]
+
+    # Guarda la raíz de las palabras (stemming of words)
+    if use_stemmer:
+        porter = PorterStemmer()
+        words = [porter.stem(word) for word in words]
+
+    # Genera una nueva lista “limpia” de oraciones
+    return words
 
 
 if __name__ == "__main__":
