9cf25c85f9db9b09d17490d35483828f5173b567
Ander Raso Vázquez <anderraso@gmail.com>
2018-11-09 16:26:53 +0100

    clase para crear heatmaps añadida
    Signed-off-by: Ander Raso Vázquez <anderraso@gmail.com>

--- python/src/heatmap.py
+++ python/src/heatmap.py
@@ -1,90 +1,102 @@
-import sys
-import csv
+""" Utiliza un csv que contiene los clusters para crear un grafico.
+
+usage: heatmap.py [-h] csv_file output_file
+
+positional arguments:
+  csv_file     Archivo csv a analizar
+  output_file  Nombre del archivo a guardar
+
+optional arguments:
+  -h, --help   show this help message and exit
+"""
+
+import os
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from argparse import ArgumentParser
+
+
+def _get_args():
+    """Devuelve los argumentos introducidos por la terminal."""
+    parser = ArgumentParser()
+    parser.add_argument('csv_file',
+                        type=str,
+                        help='Archivo csv a analizar')
+    parser.add_argument('output_file',
+                        type=str,
+                        help='Nombre del archivo a guardar')
+    return parser.parse_args()
+
+
+class HeatMapPlotter(object):
+    """Creates a HeatMap."""
+    def __init__(self, csv_file, output_file, plot_width=25, plot_height=10, plot_title="MAPA DE CALOR DE ENFERMEDADES POR CLUSTER"):
+        self.output_file = output_file
+        self.df = self._create_dataframe(csv_file)
+        self.plot_width = plot_width
+        self.plot_height = plot_height
+        self.plot_title = plot_title
+
+    def _create_dataframe(self, csv_file):
+        """Crea el dataframe con el formato correcto para hacer un HeatMap."""
+        df_base = pd.read_csv(csv_file)
+
+        lista_enfermedades = df_base["gs_text34"].unique()
+        lista_clusters = df_base["cluster"].unique()
+
+        df = pd.DataFrame(index=lista_enfermedades, columns=lista_clusters)
+        df = df.fillna(0) # rellena el dataframe con ceros
+
+        # rellena el dataframe con valores reales
+        for cluster in lista_clusters:
+            df_cluster_actual = df_base[df_base["cluster"] == cluster]  # filtrar el df por el cluster actual
+            pares = df_cluster_actual["gs_text34"].value_counts()  # {enfermedad: numero_de_casos, ...}
+            for enf, times in pares.iteritems():
+                df.at[enf, cluster] = times # rellena la columna del cluster correspondiente
+
+        df = self._ordenar_columnas(df)
+        df = self._ordenar_filas(df)
+        return df
+
+    def _ordenar_filas(self, df):
+        """Poner arriba las filas que tengan la enfermedad con el valor más alto."""
+        a = dict(df.max(axis=1))
+        sorted_by_value = sorted(a.items(), key=lambda kv: kv[1])
+        sorted_index = [item[0] for item in sorted_by_value]
+        return df.reindex(sorted_index)
+
+    def _ordenar_columnas(self, df):
+        """Poner a la izquierda las columnas que tengan la enfermedad con el valor más alto."""
+        b = dict(df.max())
+        sorted_by_value = sorted(b.items(), key=lambda kv: kv[1], reverse=True)
+        sorted_columns = [item[0] for item in sorted_by_value]
+        df = df.reindex(columns=sorted_columns)
+        return df
+
+    def crear_grafico(self):
+        """Guarda el grafico en el archivo indicado."""
+        plt.figure(figsize=(self.plot_width, self.plot_height))
+        plt.pcolor(self.df, cmap='hot_r', edgecolor='k')
+        plt.yticks(np.arange(0.5, len(self.df.index), 1), self.df.index)
+        plt.xticks(np.arange(0.5, len(self.df.columns), 1), self.df.columns, rotation=90)
+        plt.colorbar()
+        plt.title(self.plot_title)
+
+        dir_path = os.path.dirname(os.path.realpath(self.output_file))
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+            print("Created: " + dir_path)
+
+        plt.savefig(self.output_file)
+        print("Created: " + self.output_file)
 
 
 def main():
-    try:
-        file = sys.argv[1]
-        path_to_save = sys.argv[2]
-    except IndexError:
-        documentation = "Genera un heatmap dadp un archivo .csv en el que se disponga del cluster asignado a cada una" \
-                        " de las instancias. Dos argumentos esperado:\n" \
-                        "\t1 - Ruta del archivo .csv con la información de los cluster.\n" \
-                        "\t2 - Ruta donde guardar el heatmap en formato .png.\n" \
-                        "Ejemplo: python heatmap.py clustered_instances.csv /path/to/heatmap.png"
-        print(documentation)
-        sys.exit(1)
-
-    clusters = []
-    death = []
-    clusters_dict = {}
-    death_dict = {}
-
-    with open(file, 'r', newline='') as f:
-        # Create the csv reader for the input file
-        reader = csv.reader(f, delimiter=',', quotechar='\"')
-        # First line is the header row. Check the index of the cluster and the cause of death.
-        first_row = reader.__next__()
-        clusters_i = first_row.index('cluster')
-        death_i = first_row.index('gs_text34')
-        # Iterate on the remaining lines to get all posible clusters and causes of death.
-        # Also stores in a dictionary how many instances are in every cluster and cause of death.
-        for row in reader:
-            if not row[clusters_i] in clusters:
-                clusters.append(row[clusters_i])
-                clusters_dict[row[clusters_i]] = 1
-            else:
-                clusters_dict[row[clusters_i]] += 1
-            if not row[death_i] in death:
-                death.append(row[death_i])
-                death_dict[row[death_i]] = 1
-            else:
-                death_dict[row[death_i]] += 1
-
-        # Sorts the causes of death by their number of instaces.
-        sorted_death = []
-        for a, b in sorted(death_dict.items(), key=lambda x: x[1], reverse=False):
-            sorted_death.append(a)
-
-        # Sorts the clusters by their number of instaces.
-        sorted_clusters = []
-        for a, b in sorted(clusters_dict.items(), key=lambda x: x[1], reverse=True):
-            sorted_clusters.append(a)
-
-        # Creates a zero filled 2D array with dimensions of number of causes of death and number of clusters.
-        heatmap = np.zeros([len(sorted_death), len(sorted_clusters), ])
-        # Resets the reader to the start and skips the header row.
-        f.seek(0)
-        reader.__next__()
-        # Fills the 2D array with the number of appearances of each cause of death in each cluster
-        for row in reader:
-            heatmap[sorted_death.index(row[death_i]), sorted_clusters.index(row[clusters_i])] += 1
-        f.close()
-
-    fig, ax = plt.subplots()
-    plt.xticks(fontsize=3)
-    plt.yticks(fontsize=3)
-    ax.imshow(heatmap)
-
-    # We want to show all ticks...
-    ax.set_xticks(np.arange(len(sorted_clusters)))
-    ax.set_yticks(np.arange(len(sorted_death)))
-    # ... and label them with the respective list entries
-    ax.set_xticklabels(sorted_clusters)
-    ax.set_yticklabels(sorted_death)
-
-    # Rotate the tick labels and set their alignment.
-    plt.setp(ax.get_xticklabels(), rotation=90, ha="right", rotation_mode="anchor")
-
-    ax.set_title("Heatmap")
-    fig.tight_layout()
-    plt.imshow(heatmap, origin="lower", cmap='hot_r', interpolation='nearest')
-    plt.colorbar()
-    plt.savefig(path_to_save, dpi=2000)
-    plt.show()
+    args = _get_args()
+    plotter = HeatMapPlotter(args.csv_file, args.output_file)
+    plotter.crear_grafico()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
